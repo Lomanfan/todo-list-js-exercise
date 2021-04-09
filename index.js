fad8bc6a19a167e56abf4990e44679d7ab072ec1
Liuming <lfan.canada@gmail.com>
2021-04-09 20:46:48 +0000

    use object to store info & retrieve info with obj.method

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-
 function newTask(title, description) {
   const task = {
     title: title,
     description: description,
     complete: false,
 
-  logTaskState: function() {
-    console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
-  },
+    logTaskState: function () {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
 
-  markCompleted: function() {
-    this.complete = true;
+
+    printDescription: function () {
+      console.log(`Task Description:" ${this.description}`)
+    },
+
+    markCompleted: function () {
+      this.complete = true;
     }
 
   };
@@ -18,12 +22,14 @@ function newTask(title, description) {
 
 };
 
-// DRIVER CODE BELOW
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-// const task2 = newTask("Do Laundry", "😨");
-// const tasks = [task1, task2];
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
+//Logging tasks:
 task1.logTaskState(); // Clean Cat Litter has not been completed
+task2.logTaskState(); // Do Laundry has not been completed
+task1.printDescription();
+task2.printDescription();
 
-task1.markCompleted();
-task1.logTaskState(); // to print log ==> Clean Cat Litter has been completed
+
